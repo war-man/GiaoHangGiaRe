c4f69a3c55acaca31a41b730fea708e42107bc35
ngocminh2306 <ngocminh2306@gmai.com>
2017-09-22 21:22:12 -0700

    Them modoul

---
+++ GiaoHangGiaRe/GiaoHangGiaRe/Module/Don_Hang/DonHangServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models.EntityModel;
+
+namespace GiaoHangGiaRe.Module
+{
+    public class DonHangServices : IDonHangServices
+    {
+        public int Create(DonHang input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(DonHang input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DonHang> GetAll(int? page, int? size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DonHang GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DonHang> SearchByKey(int? page, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(DonHang input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GiaoHangGiaRe/GiaoHangGiaRe/Module/Don_Hang/IDonHangServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.EntityModel;
+
+namespace GiaoHangGiaRe.Module
+{
+    interface IDonHangServices
+    {
+        List<DonHang> GetAll(int? page, int? size);
+        List<DonHang> SearchByKey(int? page, string key);
+        DonHang GetById(int id);
+
+        int Create(DonHang input);
+        int Update(DonHang input);
+        int Delete(DonHang input);
+    }
+}

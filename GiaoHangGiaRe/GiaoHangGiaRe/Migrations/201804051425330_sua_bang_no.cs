6cdc94f3833ff852e365c4f6adc6ba12e7f0140d
ngocminh <ngocminh2306@gmail.com>
2018-04-12 08:33:34 +0700

    updarte

---
+++ GiaoHangGiaRe/GiaoHangGiaRe/Migrations/201804051425330_sua_bang_no.cs
@@ -0,0 +1,16 @@
+namespace GiaoHangGiaRe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sua_bang_no : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

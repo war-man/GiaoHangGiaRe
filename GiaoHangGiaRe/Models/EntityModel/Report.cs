651e160e7e1c292334bd68942f15111ced911d38
ngocminh2306 <ngocminh2306@gmai.com>
2018-04-27 14:16:53 +0700

    update

---
+++ GiaoHangGiaRe/Models/EntityModel/Report.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.EntityModel
+{
+    [Table("Report")]
+    public partial class Report
+    {
+        [Key]
+        public int Report_Id { get; set; }
+        [Required]
+        public string Report_Type { get; set; }
+        [Required]
+        public string Report_Content { get; set; }
+        public int MaDonHang { set; get; }
+        [Required]
+        public string User_id { set; get; }
+        public int MaNhanVien { set; get; }
+    }
+}

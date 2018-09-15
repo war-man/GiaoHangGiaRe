11ab7c82a79c158d7dca09811ca49bc2db225a58
ngocminh2306_destop <ngocminh2306@gmail.com>
2018-09-16 00:58:28 +0700

    a

--- GiaoHangGiaRe/GiaoHangGiaRe/Module/Don_Hang/DonHangServices.cs
+++ GiaoHangGiaRe/GiaoHangGiaRe/Module/Don_Hang/DonHangServices.cs
@@ -201,7 +201,7 @@ namespace GiaoHangGiaRe.Module
                 List<DonHangKienHang> listDonHangKienHang = new List<DonHangKienHang>();
                 var res = _donhangRepository.GetAll()
                 .Where(p => p.MaNhanVienGiao == (nhanVienServices.GetNhanVienCurrentUser().MaNhanVien) &&
-                (p.TinhTrang == DonHangConstant.Huy || p.TinhTrang == DonHangConstant.GiaoThanhCong))
+                (p.TinhTrang == DonHangConstant.Huy || p.TinhTrang == DonHangConstant.GiaoThanhCong || p.TinhTrang == DonHangConstant.KhongTheLayHang))
                 .ToList();
                 foreach (var item in res)
                 {
